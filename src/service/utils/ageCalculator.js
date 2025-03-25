697af274d9917d7a0ac6cc6a65e5aebed6c5b7e1
Dashunya Sharova <dashunya@DMac.local>
2025-03-25 15:16:00 -0600

    feat: add age calculation logic

--- src/service/utils/ageCalculator.js
+++ src/service/utils/ageCalculator.js
@@ -1,6 +1,30 @@
 export default function ageCalculator(day, month, year){
-    console.log('day', day);
-    console.log('month', month);
-    console.log('year', year);
+    const age = {};
+
+    const birthDate = new Date(year, month - 1, day); 
+    const today = new Date();
+
+    let ageYears = today.getFullYear() - birthDate.getFullYear();
+    let ageMonths = today.getMonth() - birthDate.getMonth();
+    let ageDays = today.getDate() - birthDate.getDate();
+
+    if (ageDays < 0) {
+        const previousMonth = new Date(today.getFullYear(), today.getMonth(), 0);
+        ageDays += previousMonth.getDate();
+        ageMonths--;
+    }
+
+    if (ageMonths < 0) {
+        ageMonths += 12;
+        ageYears--;
+    }
+
+    age.years = ageYears;
+    age.month = ageMonths;
+    age.days = ageDays;
+
+
+    console.log(age)
+    return age
 
 }
